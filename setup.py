4cc316122878bde25f9acf10e489e91caad383b1
iamsajidshaz <iamsajid.aa@gmail.com>
2020-09-15 14:14:43 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyShop',
+    version='1',
+    packages=['pyshop', 'products', 'products.migrations'],
+    url='',
+    license='',
+    author='iamsajidshaz',
+    author_email='',
+    description=''
+)
